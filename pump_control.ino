b1c31538451241d09e1d0ba09ac75f90bb064892
qnxxxx <qnxxxx@users.noreply.github.com>
2020-07-23 00:44:42 +0300

    Add files via upload

--- pump_control.ino
+++ pump_control.ino
@@ -2,8 +2,8 @@
 #include <Wire.h> 
 #include "DFRobot_RGBLCD.h"
 
-DFRobot_RGBLCD lcd(16,2); //set the LCD for 16 characters and 2 lines
-
+//set the LCD for 16 characters and 2 lines
+DFRobot_RGBLCD lcd(16,2);
 char line0[17];
 char line1[17];
 
@@ -22,17 +22,30 @@ WiFiServer server(80);        // start the basic web server
 
 // :::::::::: Distance sensor ::::::::::
 // Assign pin numbers:
-const int trigPin = 12;
-const int echoPin = 11;
+#define trigPin 12
+#define echoPin 11
 
 // Define variables:
-long duration;
-int distance;
+int distance, duration;
 
 // :::::::::: Pump relay ::::::::::
 // Assign pin numbers and state:
-const int relayPin = 10;
-int relayState = HIGH;
+const int pumpRelayPin = 9;
+int pumpRelayState = HIGH;
+
+// :::::::::: Valve relay ::::::::::
+// Assign pin numbers and state:
+const int valveRelayPin = 10;
+int valveRelayState = HIGH;
+
+// :::::::::: Timing ::::::::::
+// Assign timings for the loop:
+unsigned long lastRun = 0;            // will store last time Sensor was updated
+unsigned long lastCheckTime = 0;
+long onTime = 10000;                  // set milliseconds of on-time 120000
+long offTime = 30000;                 // set milliseconds of off-time 7200000
+bool isPumpWorking = false;
+
 
 
 
@@ -75,31 +88,35 @@ void setup() {
     // Wait 10 seconds for connection:
     delay(10000);
   }
-  
-  // :::::::::: Start the server ::::::::::
-  server.begin();
-  
+
   // Connected now, so print out the data:
   Serial.print("You're connected to the network ");
   printCurrentNet();
   printWifiData();
   lcdNetStatusOk();
+
+  // :::::::::: Start the server ::::::::::
+  server.begin();
   
   // :::::::::: SENSOR ::::::::::
   // Assign pin status:
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  // :::::::::: RELAY ::::::::::
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, relayState); 
+  // :::::::::: PUMP RELAY ::::::::::
+  pinMode(pumpRelayPin, OUTPUT);
+  digitalWrite(pumpRelayPin, pumpRelayState);
+
+  // :::::::::: VALVE RELAY ::::::::::
+  pinMode(valveRelayPin, OUTPUT);
+  digitalWrite(valveRelayPin, valveRelayState); 
+
 }
 
 
 
 void loop() {
-  // :::::::::: NETWORKING ::::::::::
-  
+  // :::::::::: WEB SERVER ::::::::::
   // Serial to Server:
   WiFiClient client = server.available();
   if (client) {
@@ -128,7 +145,7 @@ void loop() {
         }
         
         // Check to see if the client request was "GET /H" or "GET /L":
-        performRequest(currentLine);
+        //performRequest(currentLine);
       }
     }
     
@@ -136,42 +153,100 @@ void loop() {
     client.stop();
     Serial.println("Client disconnected");
   }
-  
-  // :::::::::: PUMP CONTROL ::::::::::
-  
+
+  // :::::::::: IRRIGATION TANK CONTROL ::::::::::
+
+  //delay(100);
+
   // Distance readout:
-  int dist = sensorReadDistance();
-  
+  int dist = sonar();
+
+  // Timing:
+  unsigned long currentMillis = millis();
+
+  // :::::::::: VALVE CONTROL ::::::::::
   // Change the desired distances in cm:
   if (dist > 200 || dist <= 0) {
-    relayState = HIGH;
-    Serial.print(dist);
-    Serial.println("cm - out of range ");
-    Serial.println("PUMP: OFF");
-    lcdSensorOut();
+    pumpRelayState = HIGH;
+    valveRelayState = HIGH;
+    serialOutOfRange();
+    lcdOutOfRange();
+    digitalWrite(pumpRelayPin, pumpRelayState);
+    digitalWrite(valveRelayPin, valveRelayState);
   }
 
   else if (dist <= 20) {
-    relayState = HIGH;
-    Serial.print(dist);
-    Serial.println("cm - tank full");
-    Serial.println("PUMP: OFF ");
-    lcdSensorFull();
+    pumpRelayState = HIGH;
+    valveRelayState = HIGH;
+    serialTankFull();
+    lcdTankFull();
+    digitalWrite(pumpRelayPin, pumpRelayState);
+    digitalWrite(valveRelayPin, valveRelayState);
  }
 
   else {
-    relayState = LOW;
-    Serial.print(dist);
-    Serial.println("cm - filling");
-    Serial.println("PUMP: ON ");
-    lcdSensorFilling();
+    valveRelayState = LOW;
+    serialValveFilling();
+    lcdValveFilling();
+    digitalWrite(valveRelayPin, valveRelayState);
   }
 
-  digitalWrite(relayPin, relayState);
+  // :::::::::: PUMP CONTROL ::::::::::
+  // Change to desired distance in cm:
+  if (lastRun == 0 || (!isPumpWorking && currentMillis >= lastRun + offTime)) {
+    int dist = sonar();
+    
+    if (200 >= dist && dist > 20) {
+      pumpRelayState = LOW;
+      serialPumpFilling();
+      lcdPumpFilling();
+      lastRun = currentMillis;
+      isPumpWorking = true;
+      digitalWrite(pumpRelayPin, pumpRelayState);
+    }
+
+  }
   
+  if(isPumpWorking && currentMillis > lastRun + onTime){
+    pumpRelayState = HIGH;
+    serialPumpWaiting();
+    lcdPumpWaiting();
+    isPumpWorking = false;
+    digitalWrite(pumpRelayPin, pumpRelayState);
+  }
+
 }
 
 
+
+// :::::::::: SERIAL CONSOLE FUNCTIONS ::::::::::
+void serialOutOfRange() {
+  Serial.print(sonar());
+  Serial.println("cm: OUT OF RANGE!!!");
+  Serial.println("|P:OFF|  |V:OFF|");
+}
+
+void serialTankFull() {
+  Serial.print(sonar());
+  Serial.println("cm: TANK FULL!!!");
+  Serial.println("|P:OFF|  |V:OFF|");
+}
+
+void serialPumpFilling() {
+  Serial.print(sonar());
+  Serial.println("cm: FILLING...");
+  Serial.println("PUMP: ON");
+}
+
+void serialValveFilling() {
+  Serial.print(sonar());
+  Serial.println("cm: FILLING...");
+  Serial.println("VALVE: ON");
+}
+
+void serialPumpWaiting() {
+  Serial.println("PUMP: WAITING...");
+}
 
 // :::::::::: LCD FUNCTIONS ::::::::::
 void lcdNetInit() {
@@ -188,8 +263,8 @@ void lcdNetStatusConnecting() {
   // print network connecting message to LCD
   lcd.clear();
   lcd.setRGB(255,255,0);
-  lcd.setCursor(1,0);
-  lcd.print(F("CONNECTING TO:"));
+  lcd.setCursor(0,0);
+  lcd.print(F(" CONNECTING TO: "));
   lcd.setCursor(5,1);
   lcd.print(ssid);
   lcd.blink();
@@ -200,7 +275,7 @@ void lcdNetStatusOk() {
   lcd.clear();
   lcd.setRGB(0,255,0);
   lcd.setCursor(0,0);
-  lcd.print(F(" CONNECTED TO:"));
+  lcd.print(F(" CONNECTED  TO: "));
   lcd.setCursor(5,1);
   lcd.print(WiFi.SSID());
   delay(5000);
@@ -229,33 +304,44 @@ void lcdNetStatusWiFiModuleFirmware() {
   lcd.blink();
 }
 
-void lcdSensorOut() { 
-  lcd.clear();
-  lcd.setRGB(255,0,0);
-  lcd.setCursor(2,0);
-  lcd.print(F("Out of range"));
-  lcd.setCursor(3,1);
-  lcd.print(F("PUMP: OFF"));
-}
-
-void lcdSensorFull() {
-  lcd.clear();
+void lcdOutOfRange() { 
   lcd.setRGB(255,0,0);
   lcd.setCursor(0,0);
-  lcd.print(sensorReadDistance());
-  lcd.println(F("cm - tank full"));
-  lcd.setCursor(3,1);
-  lcd.print(F("PUMP: OFF"));
+  lcd.print(F(" OUT OF RANGE!!!"));
+  lcd.setCursor(0,1);
+  lcd.print(F("|P:OFF|  |V:OFF|"));
 }
 
-void lcdSensorFilling() {
-  lcd.clear();
+void lcdTankFull() {
+  lcd.setRGB(255,0,0);
+  lcd.setCursor(0,0);
+  lcd.print(sonar());
+  lcd.println(F("cm: TANK FULL!!!"));
+  lcd.setCursor(0,1);
+  lcd.print(F("|P:OFF|  |V:OFF|"));
+}
+
+void lcdPumpFilling() {
   lcd.setRGB(0,255,0);
   lcd.setCursor(0,0);
-  lcd.print(sensorReadDistance());
-  lcd.println(F("cm - filling"));
-  lcd.setCursor(4,1);
-  lcd.print(F("PUMP: ON"));
+  lcd.print(sonar());
+  lcd.println(F("cm: FILLING..."));
+  lcd.setCursor(0,1);
+  lcd.print(F("|P: ON| "));
+}
+
+void lcdValveFilling() {
+  lcd.setRGB(0,255,0);
+  lcd.setCursor(0,0);
+  lcd.print(sonar());
+  lcd.println(F("cm: FILLING..."));
+  lcd.setCursor(8,1);
+  lcd.print(F(" |V: ON|"));
+}
+
+void lcdPumpWaiting() { 
+  lcd.setCursor(0,1);
+  lcd.print(F("|P:WAIT|"));
 }
 
 // :::::::::: NETWORK FUNCTIONS ::::::::::
@@ -281,50 +367,97 @@ void printCurrentNet() {
 void showWebPage(WiFiClient client) {
   // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
   // and a content-type so the client knows what's coming, then a blank line:
-  client.println("HTTP/1.1 200 OK");
+  client.println("HTTP/3 200 OK");
   client.println("Content-type:text/html");
   client.println();
 
   // the content of the HTTP response follows the header:
-  client.println("<h1>PUMP Remote Control</h1>");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<head>");
+  client.println("<meta http-equiv='refresh' content='10'>");
+  client.println("<link href='https://fonts.googleapis.com/css2?family=Share+Tech+Mono&amp;display=swap' rel='stylesheet'>");
+  client.println("</head>");
+  client.println("<body style='background-color:LightGrey;'>");
+  client.println("<h1>IRRIGATION TANK REMOTE CONTROL</h1>");
   client.println("<table border=1 style='text-align:center'>");
   client.println("<tr><th>Component</th><th>Status</th><th>Control</th></tr>");
 
   // PUMP relay:
   client.print("<tr><td>PUMP</td><td>");
-  if (digitalRead(relayPin)) {
+  if (digitalRead(pumpRelayPin)) {
+    client.print("<font style='font-family:'Share Tech Mono'; color:red;'>OFF</font>");
+  } else {
+    client.print("<font style='color:green;'>ON</font>");
+  }
+  client.println("</td><td><a href='/pump_relay/on'>ON</a> / <a href='/pump_relay/off'>OFF</a></td></tr>");
+
+  // VALVE relay:
+  client.print("<tr><td>VALVE</td><td>");
+  if (digitalRead(valveRelayPin)) {
     client.print("<font style='color:red;'>OFF</font>");
   } else {
     client.print("<font style='color:green;'>ON</font>");
   }
-  client.println("</td><td><a href='/relay/on'>ON</a> / <a href='/relay/off'>OFF</a></td></tr>");
+  client.println("</td><td><a href='/valve_relay/on'>ON</a> / <a href='/valve_relay/off'>OFF</a></td></tr>");
 
   client.println("</table>");
 
-  // The HTTP response ends with another blank line
+  // Level meter:
+  int level = sonar();
+
+  client.println("<h1>IRRIGATION TANK CURRENT LEVEL</h1>");
+  client.println("<table border=1 style='text-align:center'>");
+  client.println("<tr><th>Tank</th><th>Level</th></tr>");
+
+  client.print("<tr><td>IRRIGATION TANK</td><td>");
+  if (200 >= level && level > 100) {
+    client.print("<font style='color:red;'>");
+    client.print(level);
+    client.print("</font><font style='color:black;>'>cm</font>");
+  } else if (100 >= level && level > 50) {
+    client.print("<font style='color:gold;'>");
+    client.print(level);
+    client.print("</font><font style='color:black;>'>cm</font>");
+  } else if (50 >= level && level > 20) {
+    client.println("<font style='color:green;'>");
+    client.print(level);
+    client.print("</font><font style='color:black;>'> cm</font>");
+  } else {
+    client.println("<font style='color:pink;'>ERROR</font>");
+  }
+  
+  // The HTTP response ends with another blank line:
   client.println();
 }
 
-void performRequest(String line) {
-  if (line.endsWith("GET /relay/on")) {
-    digitalWrite(relayPin, LOW);
-  } else if (line.endsWith("GET /relay/off")) {
-    digitalWrite(relayPin, HIGH);
-  }
-}
+//void performRequest(String line) {
+  //if (line.endsWith("GET /pump_relay/on")) {
+    //digitalWrite(pumpRelayPin, LOW);
+  //} else if (line.endsWith("GET /pump_relay/off")) {
+    //digitalWrite(pumpRelayPin, HIGH);
+  //} else if (line.endsWith("GET /valve_relay/on")) {
+    //digitalWrite(valveRelayPin, LOW);
+  //} else if (line.endsWith("GET /valve_relay/off")) {
+    //digitalWrite(valveRelayPin, HIGH);
+  //}
+//}
 
 // :::::::::: SENSOR FUNCTIONS ::::::::::
-int sensorReadDistance() {
+int sonar() {
   // Clear the trigPin:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+  
   // Send signal:
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+
   // Take a reading:
   duration = pulseIn(echoPin, HIGH);
+  
   // Calculate and return the distance:
-  distance = (duration/2)/29.154;
+  distance = (duration/2)/29.154; 
   return distance;
 }
